511816e59cba93399b0df7b87b0638b6aa500045
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-05 07:55:12 +0100

    task 9 - removed if from the for loop statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,13 +3,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        max_val = None
+        max_val = ("None")
     else:
         max_val = 0
         for i in my_list:
-            if len(my_list) == 0:
-                max_val = None
-                return max_val
             if i > max_val:
                 max_val = i
         return max_val
