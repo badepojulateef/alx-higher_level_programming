b52b69e37e32d2745f96336d791134b16957578d
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-05 03:06:35 +0100

    added task 0 - 14

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        max_val = None
+    else:
+        max_val = 0
+        for i in my_list:
+            if len(my_list) == 0:
+                max_val = None
+                return max_val
+            if i > max_val:
+                max_val = i
+        return max_val
