2cd631d384bd49684a1f523c511fdaf5d0e7b6c8
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-05 07:59:02 +0100

    task 9 - modified the implementation

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        max_val = ("None")
+        max_val = None
     else:
-        max_val = 0
-        for i in my_list:
+        max_val = my_list[0]
+        for i in range(len(my_list)):
             if i > max_val:
-                max_val = i
+                max_val = max_list[i]
         return max_val
