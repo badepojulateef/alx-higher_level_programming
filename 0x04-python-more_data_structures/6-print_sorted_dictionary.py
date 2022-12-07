885115b834fa4032951841c822b1283a3e22b585
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-07 23:40:46 +0100

    modified return statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    print(dict(sorted(a_dictionary.items())))
-    return
+    return print(dict(sorted(a_dictionary.items())))
+    
