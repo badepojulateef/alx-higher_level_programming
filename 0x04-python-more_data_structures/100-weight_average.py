1f65196642a647b5af993daa86008dee901e2a95
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-08 04:43:08 +0100

    added if statement to task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,9 @@
 def weight_average(my_list=[]):
     x = 0
     y = 0
-    for tuple in my_list:
-        x += (tuple[0] * tuple[1])
-        y += tuple[1]
-    return (x/y)
+    if my_list and len(my_list) != 0:
+        for tuple in my_list:
+            x += (tuple[0] * tuple[1])
+            y += tuple[1]
+        return (x/y)
+    return 0
