f440719f8d700d191e1c1884008c3d12ab4b60a2
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-19 15:49:45 +0100

    removed empty space from line 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     counter = 0
     for i in range(x):
@@ -7,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             counter += 1
         except:
-            break
+            pass
     print()
     return counter
