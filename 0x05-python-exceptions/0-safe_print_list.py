02320fd050e114191bf39c6e85657250f6ecd3af
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-19 16:02:30 +0100

    modified task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             counter += 1
         except:
-            pass
+            break
     print()
     return counter
