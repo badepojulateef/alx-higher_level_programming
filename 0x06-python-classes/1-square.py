71188400aace2d10526d37f3cf622bbd1d00cb3f
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-20 23:47:48 +0100

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py by Adepoju Lateef
+""" Defines a class Square """
+
+
+class Square:
+    """ Defines a square class """
+    
+    def __init__(self, size):
+        """ Initializing the sqauare class
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
