1dc0d7f5dc3e2637825ec56980730ac9e905a9b1
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-21 03:54:30 +0100

    removed some blank spaces

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A module that defines a square """
+"""A module that defines a square."""
 
 
 class Square:
     """A class that represents a square"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initializing this square class
         Args:
             size: represnets the size of the square defined
