7d768bf9d970ff68fa26b1abfe759f32b05f3393
Adepoju Lateef <badepojulateef@gmail.com>
2023-01-10 04:55:18 +0100

    removed blank line from task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,6 @@
 
 class MyList(list):
     """ Defines a subclass of class """
-
     def print_sorted(self):
         """ prints the a sorted list in an ascending sort """
         print(sorted(self))
