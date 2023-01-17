20e99346d3bf882e3ef3cfd11267643689333d9a
Adepoju Lateef <badepojulateef@gmail.com>
2023-01-17 02:15:56 +0100

    corrected EOL error from task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,5 +97,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return f"[Rectangle]({self.__id}) {self.__x}/{self.__y} -
-        {self.__width}/{self.__height}
+        return f"[Rectangle]({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
