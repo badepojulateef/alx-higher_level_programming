0320f80c274e0d93ef44fd2b0053d3ebf044ca73
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-14 22:53:03 +0100

    debugged task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,26 +4,26 @@ const fs = require('fs');
 
 async function concatFiles () {
   try {
-    if (process.argv.length !== 5) {
-      console.log('Usage: node concat.js file1 file2 dest');
+    if (process.argv.length < 3) {
+      console.log('Usage1: node concat.js file1 file2 dest');
       return;
     }
 
-    const [, , file1, file2, dest] = process.argv;
+    const [, , dest, file1, file2] = process.argv;
 
-    const readStream1 = fs.createReadStream(file1);
-    const readStream2 = fs.createReadStream(file2);
+    const readStream1 = file1 && fs.createReadStream(file1);
+    const readStream2 = file2 && fs.createReadStream(file2);
     const writeStream = fs.createWriteStream(dest, { flags: 'a' });
 
-    await readStream1.pipe(writeStream);
-    await readStream2.pipe(writeStream);
+    await readStream1 && readStream1.pipe(writeStream);
+    await readStream2 && readStream2.pipe(writeStream);
 
     writeStream.on('finish', () => {
-      console.log('Usage: node concat.js file1 file2 dest');
+      // console.log('Usage2: node concat.js file1 file2 dest');
       console.log(`Successfully concatenated ${file1} and ${file2} to ${dest}`);
     });
   } catch (err) {
-    console.error(err);
+    console.log('err =>', err);
   }
 }
 
