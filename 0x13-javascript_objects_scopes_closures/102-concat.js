64ab60c9a7af2b8021b92ca768c35bc6ef0a6868
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-15 05:23:10 +0100

    corrected task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,18 +6,13 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-if (
-  fs.existsSync(fileA) &&
-fs.statSync(fileA).isFile &&
-fs.existsSync(fileB) &&
-fs.statSync(fileB).isFile &&
-fileC !== undefined
-) {
-  const fileAContent = fs.readFileSync(fileA);
-  const fileBContent = fs.readFileSync(fileB);
+if (fs.existsSync(fileA) && fs.statSync(fileA).isFile &&
+    fs.existsSync(fileB) && fs.statSync(fileB).isFile && fileC !== undefined) {
+  const fileAreadFileSync = fs.readFileSync(fileA);
+  const fileBreadFileSync = fs.readFileSync(fileB);
   const stream = fs.createWriteStream(fileC);
 
-  stream.write(fileAContent);
-  stream.write(fileBContent);
+  stream.write(fileAreadFileSync);
+  stream.write(fileBreadFileSync);
   stream.end();
 }
