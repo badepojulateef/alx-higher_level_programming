0aafacde48d900eccf26e064c9699e95f4e85903
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-19 05:52:59 +0100

    implemented when states are empty in task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -33,7 +33,10 @@ if __name__ == "__main__":
     state = session.query(State).order_by(State.id).first()
 
     # Display the results
-    print("{}: {}".format(state.id, state.name))
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
 
     # Close the session
     session.close()
