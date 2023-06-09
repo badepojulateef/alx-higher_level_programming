f2ee770ee1f5c088856827cd9147d1602b7f5e55
Adepoju Lateef <111166902+badepojulateef@users.noreply.github.com>
2023-06-09 11:51:15 +0100

    added space to the output from __str__ rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,7 +97,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return f"[Rectangle]({self.id}) {self.__x}/{self.__y} - \
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
 {self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
