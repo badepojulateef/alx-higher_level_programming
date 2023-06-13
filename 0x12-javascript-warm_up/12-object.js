5afcbb304e40a8d0bd25ee44d63d58d9f34f0474
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 00:41:34 +0100

    added task 12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that searches the second biggest integer
+// in the list of arguments.
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
