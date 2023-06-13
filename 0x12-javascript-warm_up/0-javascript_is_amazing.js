df36bdc46b89c73010a41274291034f7c50383e8
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 00:36:52 +0100

    added task 0

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script to print the text "JavaScript is amazing"
+
+// Define a constant variable myVar with the value "JavaScript is amazing"
+const myVar = 'JavaScript is amazing';
+// Print the value of myVar to the console
+console.log(myVar);
