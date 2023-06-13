8b5e22fb945fa272aa94c07edf13b5416b7695a4
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 00:37:54 +0100

    added task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script to print the text "JavaScript is amazing"
+
+// Print the value of myVar to the console
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
