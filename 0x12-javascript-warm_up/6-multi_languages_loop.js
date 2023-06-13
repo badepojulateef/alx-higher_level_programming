270bc7085b91ff77d58d106f5dbebb7471c50368
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 00:39:02 +0100

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop ”"
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (arr[i]) {
+  console.log(arr[i]);
+  i += 1;
+}
