e09a4ffb08d1d57c2ba2e3986d67f5663646e0c1
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 05:43:02 +0100

    corrected task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // Script that prints a message depending of the number of arguments passed"
 
-const { argv } = require('node:process');
-
-console.log(argv[2] || 'No argument');
+console.log(process.argv[2] || 'No argument');
