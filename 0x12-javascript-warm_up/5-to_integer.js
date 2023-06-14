eb39aa23fb8cbd6d6b9a1a2257cca647f353caf4
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 05:45:31 +0100

    corrected task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Script that prints two arguments passed to it, in the following format: “ is ”"
 
-const { argv } = require('node:process');
-const arg = parseInt(argv[2]);
+const arg = parseInt(process.argv[2]);
 
 console.log(Number.isInteger(arg) ? `My number: ${arg}` : 'Not a number');
