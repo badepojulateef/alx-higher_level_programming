bf6a20cb0361fc9a8e388e65b76794b4e2244b96
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-14 05:48:39 +0100

    corrected task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // Script that computes and prints a factorial
 
-const { argv } = require('node:process');
-
 function factorial (n) {
   return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(Number(argv[2])));
+console.log(factorial(Number(process.argv[2])));
