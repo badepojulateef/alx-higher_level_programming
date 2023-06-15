9f78a7406e277a7a6394b78d0ab92a4a91b42465
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-15 01:13:42 +0100

    corrected task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const BaseSquare = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends BaseSquare {
   constructor (size) {
     super(size, size);
   }
