3b9e3b7f603704fc327e3ffb94747f441318d775
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-17 08:26:37 +0100

    added task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the
+database hbtn_0e_0_usa using sqlalchemy
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check that the script is being called correctly
+    if (len(sys.argv) != 4):
+        print("Usage: {} username password db_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Parse the command line arguments and
+    # Get MySQL credentials from command line
+    # arguments
+    username, password, db_name = sys.argv[1:4]
+    host = "localhost"
+    driver_name = "mysql+mysqldb"
+
+    # Connect to MySQL server
+    engine = create_engine(
+                "{}://{}:{}@{}/{}"
+                .format(
+                    driver_name,
+                    username,
+                    password,
+                    host,
+                    db_name
+                ),
+                pool_pre_ping=True
+            )
+
+    # Create the table(s) associated with the defined classes
+    Base.metadata.create_all(engine)
+
+    # Create a session factory bound to the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query a session factory bound to the engine
+    try:
+        states = session.query(State).filter(State.name.like('%a%'))\
+                 .order_by(State.id)
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    # Close the cursor abd database connection
+    finally:
+        session.close()
