3139f650882fa7ad031ad1c66fab179cbcd7bcd3
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-03 03:04:12 +0100

    added tash 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://intranet.hbtn.io/status
+using urllib and displays the res body
+"""
+
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    def getStatus(url):
+        """ A function that fetches the content of a url
+        and displays its res
+        
+
+        Args:
+            url (str): The URL to fetch
+
+        Returns:
+            None: The function prints but not returns nothing
+
+        """
+
+        # Open the URL as a response object
+        with urlopen(open) as res:
+            
+            # Read the contents of the response
+            status = re.read()
+
+            # Display the res
+            print("Body response:")
+            print("\t- type: {}".format(type(status)))
+            print("\t- content: {}".format(status))
+            print("\t- utf8 content: {}".format(status.decode('utf-8')))
+
+        # URL to fetch
+        url = "https://alx-intranet.hbtn.io/status"
+
+        # Call the getStatus function with the URL
+        getStatus(url)
