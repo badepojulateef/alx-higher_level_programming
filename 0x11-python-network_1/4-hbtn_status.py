d694d4a8cfd4455c34fcc7b972c88818d278491a
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-03 04:12:51 +0100

    Added task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status.
+
+Usage: ./4-hbtn_status.py
+
+Returns:
+    The body of the response to the request, as a string.
+
+Example:
+    ./4-hbtn_status.py
+
+Features:
+    - Uses the Python requests library to make a GET
+    request to a specified URL.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    # Make a GET request to the URL using the requests library
+    res = requests.get(url)
+
+    # Print out the response body
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
