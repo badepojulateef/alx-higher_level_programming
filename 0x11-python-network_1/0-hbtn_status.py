0e9744b635c5b31cd25f934fc6757c6568fde504
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-03 03:20:32 +0100

    Updated task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         with urlopen(url) as res:
 
             # Read the contents of the response
-            status = re.read()
+            status = res.read()
 
             # Display the res
             print("Body response:")
