e7950974bb9848c0bac98204732c13e75bbf1434
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-03 04:29:49 +0100

    Added task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+
+import sys
+import requests
+
+
+if __name == "__main__":
+    # Get URL from command-line arguments
+    url = sys.argv[1]
+
+    # Send a GET request to the specified URL
+    res = requests.get(url)
+
+    # Check if the response status code is an error code (>= 400)
+    if res.status_code >= 400:
+        # Print an error message with the status code
+        print("Error code: {}".format(res.status_code))
+
+    else:
+        # Print the response body
+        print(res.text)
