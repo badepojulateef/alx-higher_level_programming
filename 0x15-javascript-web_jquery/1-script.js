203586b1ce3883c1a16c49e7be353613ba911975
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-02 18:38:08 +0100

    Updated task 1

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Use JQuery ready() method to ensure the DOM is ready before executing the script.
+$(document).ready(function () {
+  // Use JQuery selector to target the <header> element and set its text color to red (#FF0000).
+  $('header').css('color', '#FF0000');
+});
