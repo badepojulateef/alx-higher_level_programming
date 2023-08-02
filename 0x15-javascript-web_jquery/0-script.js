182c37263b20d3237c1c723bda89b01b926c3d40
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-02 17:23:10 +0100

    Updated task 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// 0-script.js
+
+// 1. Select the header element using querySelector
+const headerElement = document.querySelector('header');
+
+// 2. Add a CSS class to the header element to set the text color to red
+headerElement.style.color = '#FF0000';
